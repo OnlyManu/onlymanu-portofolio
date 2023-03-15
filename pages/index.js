a9cef6f249556e60ee19aba51809f491fbf363fe
emmanuel <blimanuevil@gmail.com>
2023-03-15 19:31:57 +0100

    modifictaion de la taille max des noeuds particles changement d'imege de screenshot

--- pages/index.js
+++ pages/index.js
@@ -97,7 +97,7 @@ export default function Home() {
                 type: "circle",
             },
             size: {
-                value: { min: 1, max: 5 },
+                value: { min: 1, max: 3 },
             },
         },
         detectRetina: true,

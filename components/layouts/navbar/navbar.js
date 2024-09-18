f5c00b146df4b61189540fbcd8bdab0c55479e0b
OnlyManu <blimanuevil@gmail.com>
2024-09-18 13:47:10 +0000

    Correction de la navigitaon avec smooth-scrollbar

--- components/layouts/navbar/navbar.js
+++ components/layouts/navbar/navbar.js
@@ -1,80 +1,113 @@
-import Image from 'next/image'
-import { useState, useEffect } from 'react'
+import Image from "next/image";
+import { useState, useEffect } from "react";
 
-import BurgerButton from '../../ui/burgerButton/burgerButton'
-import Navigation from '../../ui/navigation/navigation'
-import styles from './navbar.module.css'
+import BurgerButton from "../../ui/burgerButton/burgerButton";
+import Navigation from "../../ui/navigation/navigation";
+import styles from "./navbar.module.css";
 
 export default function Navbar() {
-    const [navigationStatus, setNavigationStatus] = useState(false)
-    const [scrollPos, setScrollPos] = useState(0)
+  const [navigationStatus, setNavigationStatus] = useState(false);
+  const [scrollPos, setScrollPos] = useState(0);
 
-    const openCloseNavigation = () => {
-        const body = document.querySelector('body')
-        if (navigationStatus) {
-            setNavigationStatus(false)
-            body.classList.remove('navigationOpen')
-        } else {
-            setNavigationStatus(true)
-            body.classList.add('navigationOpen')
-        }
+  const openCloseNavigation = () => {
+    const body = document.querySelector("body");
+    if (navigationStatus) {
+      setNavigationStatus(false);
+      body.classList.remove("navigationOpen");
+    } else {
+      setNavigationStatus(true);
+      body.classList.add("navigationOpen");
+    }
+  };
+
+  useEffect(() => {
+    const container = document.querySelector("#my-scrollbar");
+    function matrixToArray(str) {
+      return str.split("(")[1].split(")")[0].split(",");
     }
 
-    useEffect(() => {
-        window.addEventListener("scroll", () => {
-            const home = document.getElementById("home")
-            const sections = document.querySelectorAll('section')
-            const links = document.querySelectorAll('nav li a')
-            const navbar = document.getElementById('navbar')
-            
-            if (window.scrollY > 250 && window.scrollY < scrollPos) {
-                navbar.classList.add('sticky')
-            } else {
-                navbar.classList.remove('sticky')
+    /*container.addEventListener("transitionend", (event) => {
+      const home = document.getElementById("home");
+      const sections = document.querySelectorAll("section");
+      const links = document.querySelectorAll("nav li a");
+      const navbar = document.getElementById("navbar");
+      const scroll_content = container.firstElementChild;
+      const transform = matrixToArray(
+        getComputedStyle(scroll_content, null).transform
+      );
+      const current_position = Math.abs(transform[5]);
+
+      if (current_position > 250 && current_position < scrollPos) {
+        navbar.classList.add("sticky");
+      } else {
+        navbar.classList.remove("sticky");
+      }
+
+      setScrollPos(current_position);
+    });*/
+
+    /*container.addEventListener(
+      "scroll",
+      () => {
+        const home = document.getElementById("home");
+        const sections = document.querySelectorAll("section");
+        const links = document.querySelectorAll("nav li a");
+        const navbar = document.getElementById("navbar");
+
+        if (window.scrollY > 250 && window.scrollY < scrollPos) {
+          navbar.classList.add("sticky");
+        } else {
+          navbar.classList.remove("sticky");
+        }
+
+        let top = window.scrollY;
+        let offset = home.offsetTop - 150;
+        let height = home.offsetHeight;
+        let id = home.getAttribute("id");
+
+        if (top >= offset && top < offset + height) {
+          links.forEach((link) => {
+            link.getAttribute("href") === "#" + id
+              ? link.parentNode.classList.add("active")
+              : link.parentNode.classList.remove("active");
+          });
+        } else {
+          sections.forEach((section) => {
+            top = window.scrollY;
+            offset = section.offsetTop - 150;
+            height = section.offsetHeight;
+            id = section.getAttribute("id");
+
+            if (top >= offset && top < offset + height) {
+              links.forEach((link) => {
+                link.getAttribute("href") === "#" + id
+                  ? link.parentNode.classList.add("active")
+                  : link.parentNode.classList.remove("active");
+              });
             }
+          });
+        }
 
-            let top = window.scrollY;
-            let offset = home.offsetTop-150;
-            let height = home.offsetHeight;
-            let id = home.getAttribute('id');
+        setScrollPos(window.scrollY);
+      },
+      false
+    );*/
+  }, [scrollPos]);
 
-            if (top >= offset && top < offset + height) { 
-                links.forEach(link => {
-                    link.getAttribute('href') === '#'+id ? link.parentNode.classList.add('active') : link.parentNode.classList.remove('active')
-                })
-            } else {
-                sections.forEach(section => {
-                    top = window.scrollY;
-                    offset = section.offsetTop-150;
-                    height = section.offsetHeight;
-                    id = section.getAttribute('id');
-
-                    if (top >= offset && top < offset + height) {
-                        links.forEach(link => {
-                            link.getAttribute('href') === '#'+id ? link.parentNode.classList.add('active') : link.parentNode.classList.remove('active')
-                        })
-                    }
-                })
-            }
-
-            setScrollPos(window.scrollY)
-        }, false)
-    }, [scrollPos])
-    
-    return (
-        <div className={styles.navbar} id="navbar">
-            <a href="#">
-                <Image
-                    className={styles.logo}
-                    src="/images/logo-name.svg"
-                    width={124}
-                    height={41.89}
-                    alt=""
-                    priority={true}
-                />
-            </a>
-            <BurgerButton isOpen={navigationStatus} onClick={openCloseNavigation}/>
-            <Navigation isOpen={navigationStatus} onClick={openCloseNavigation}/>
-        </div>
-    )
+  return (
+    <div className={styles.navbar} id="navbar">
+      <a href="#">
+        <Image
+          className={styles.logo}
+          src="/images/logo-name.svg"
+          width={124}
+          height={41.89}
+          alt=""
+          priority={true}
+        />
+      </a>
+      <BurgerButton isOpen={navigationStatus} onClick={openCloseNavigation} />
+      <Navigation isOpen={navigationStatus} onClick={openCloseNavigation} />
+    </div>
+  );
 }
--- components/ui/navigation/navigation.js
+++ components/ui/navigation/navigation.js
@@ -1,26 +1,59 @@
-import { useState } from 'react'
-import styles from './navigation.module.css'
-import utils from '../../../styles/utils.module.css'
+import { useState } from "react";
+import Scrollbar from "smooth-scrollbar";
+import styles from "./navigation.module.css";
+import utils from "../../../styles/utils.module.css";
 
-function NavLink({ children, href, home, onClick}) {
-    return (
-        <li className={home ? styles.link+" active" : styles.link} onClick={onClick}><a href={href}>{ children }</a></li>
-    )
+function NavLink({ children, href, home, onClick }) {
+  const scrollTo = (event) => {
+    event.preventDefault();
+    const href = /(#.*)/.exec(event.target.href)[0];
+    const myScrollbar = Scrollbar.get(document.querySelector("#my-scrollbar"));
+    onClick();
+    setTimeout(() => {
+      myScrollbar.scrollIntoView(document.querySelector(href));
+    }, 500);
+  };
+
+  return (
+    <li className={home ? styles.link + " active" : styles.link}>
+      <a href={href} onClick={scrollTo}>
+        {children}
+      </a>
+    </li>
+  );
 }
 
-export default function Navigation({isOpen, onClick}) {
-    const [selectedLink, setSelected] = useState("Home")
+export default function Navigation({ isOpen, onClick }) {
+  const [selectedLink, setSelected] = useState("Home");
 
-    return (
-        <nav className={isOpen ? styles.navigation + " " + styles.open + " " + utils.bgColorDark : styles.navigation + " " + utils.bgColorDark}>
-            <ul className={styles.links}>
-                <NavLink href="#home" onClick={onClick} home>Home</NavLink>
-                <NavLink href="#services" onClick={onClick}>Services</NavLink>
-                <NavLink href="#about" onClick={onClick}>About</NavLink>
-                <NavLink href="#works" onClick={onClick}>Works</NavLink>
-                <NavLink href="#skills" onClick={onClick}>Skills</NavLink>
-                <NavLink href="#contact" onClick={onClick}>Contact</NavLink>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav
+      className={
+        isOpen
+          ? styles.navigation + " " + styles.open + " " + utils.bgColorDark
+          : styles.navigation + " " + utils.bgColorDark
+      }
+    >
+      <ul className={styles.links}>
+        <NavLink href="#home" onClick={onClick} home>
+          Home
+        </NavLink>
+        <NavLink href="#services" onClick={onClick}>
+          Services
+        </NavLink>
+        <NavLink href="#about" onClick={onClick}>
+          About
+        </NavLink>
+        <NavLink href="#works" onClick={onClick}>
+          Works
+        </NavLink>
+        <NavLink href="#skills" onClick={onClick}>
+          Skills
+        </NavLink>
+        <NavLink href="#contact" onClick={onClick}>
+          Contact
+        </NavLink>
+      </ul>
+    </nav>
+  );
 }
